304cb65d165878cf5685b3bd37bc8799798845d8
Marcelo Moro <101741773+marcelomoro1@users.noreply.github.com>
2024-09-10 19:51:25 -0300

    Update PROCEDURES.sql

--- PROCEDURES EXEMPLOS/PROCEDURES.sql
+++ PROCEDURES EXEMPLOS/PROCEDURES.sql
@@ -37,3 +37,24 @@ BEGIN
 
 END
 EXEC info @Nome_departamento = 'Pesquisa' --exibe todos os funcionarios e de um departamento especifico
+
+
+CREATE PROCEDURE atualiza_salario
+@Cpf CHAR(11),
+@Novo_Salario DECIMAL(10,2)
+AS
+BEGIN
+	IF EXISTS(SELECT 1 FROM FUNCIONARIO AS F WHERE F.Cpf = @Cpf)
+		BEGIN
+
+			UPDATE FUNCIONARIO
+			SET Salario = @Novo_Salario
+			WHERE Cpf = @Cpf;
+
+		END
+	ELSE
+		PRINT 'Nao existe alguem com esse cpf';
+
+END
+EXEC atualiza_salario @Cpf = '33344555587', @Novo_Salario = 25000.00; -- Atualiza o salario de alguem com o cpf especifico e caso o cpf não exista de uma mensagem
+SELECT * FROM FUNCIONARIO WHERE FUNCIONARIO.Cpf = '33344555587';
