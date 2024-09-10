16ea1cfacbc00231e6ae2b211e97a52ceece7b22
Marcelo Moro <101741773+marcelomoro1@users.noreply.github.com>
2024-09-10 19:06:51 -0300

    Update PROCEDURES.sql

---
+++ PROCEDURES EXEMPLOS/PROCEDURES.sql
@@ -0,0 +1,17 @@
+CREATE PROCEDURE ExibirNome 
+(@Nome VARCHAR(50))
+AS
+BEGIN
+	SELECT @Nome as 'Nome';
+END
+EXEC ExibirNome @Nome = 'Marcelo'; --exibe o nome Marcelo
+
+CREATE PROCEDURE info_funcionarios
+AS
+BEGIN
+	SELECT F.Pnome, F.Unome, D.Dnome
+	FROM FUNCIONARIO AS F
+	INNER JOIN DEPARTAMENTO AS D
+	ON F.Dnr = D.Dnumero
+END
+EXEC info_funcionarios --exibe todos os funcionarios e seus respectivos departamentos
