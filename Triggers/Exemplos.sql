07d9c45ff82e787303cca8151413657bafbd5da9
Marcelo Moro <101741773+marcelomoro1@users.noreply.github.com>
2024-10-15 19:32:36 -0300

    Update Exemplos.sql

---
+++ Triggers/Exemplos.sql
@@ -0,0 +1,24 @@
+CREATE TRIGGER trg_after_insert_funcionario
+ON FUNCIONARIO
+AFTER INSERT 
+AS
+BEGIN
+	DECLARE @Cpf CHAR(11),
+			@Pnome VARCHAR(15),
+			@Unome VARCHAR (15),
+			@Salario DECIMAL (10,2);
+
+	SELECT @Cpf = I.Cpf, @Pnome = I.Pnome, @Unome = I.Unome, @Salario = I.Salario
+	FROM inserted as I
+
+	PRINT 'Funcionarios INSERIDOS: ';
+	PRINT 'CPF: ' + @Cpf;
+	PRINT 'Nome: '+ @Pnome;
+	PRINT 'Unome: '+ @Unome;
+	PRINT 'Salario: '+ CAST(@Salario AS VARCHAR(20));
+END
+
+INSERT INTO FUNCIONARIO (Cpf, Pnome, Unome) VALUES ( '12345676543', 'Mffdf', 'dfhfh');
+
+-----------------------------------------------------------------------------------------
+
